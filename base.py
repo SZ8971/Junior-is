105a787854a376f27f85b4c6b4ee721d8d45c404
SZ8971 <zsukura@gmail.com>
2024-04-15 12:41:16 -0400

    Final

--- base.py
+++ base.py
@@ -1,6 +1,13 @@
+# The creating window part is typed by meself
+# Selecting folder and file part is typed by myself
+# Detection part is comes from https://www.youtube.com/watch?v=hg4oVgNq7Do&t=358s & https://www.youtube.com/watch?v=V62M9d8QkYM&t=51s
+# The difference is I did not use the numpy from the first one and I changed the videocapture from picture to the live camera.
 import tkinter
+from tkinter import filedialog
 import cv2
 from ultralytics import YOLO
+
+
 
 # Create a window with a width of 400 and a height of 500
 root = tkinter.Tk()
@@ -26,19 +33,43 @@ def live():
             results = model(frame)
             annotated_frame = results[0].plot()
             cv2.imshow("Object Detection", annotated_frame)
-            if cv2.waitKey(1) & 0xff == ord('q'):
-                break  
+            if cv2.waitKey(1) & 0xf == ord('q'):
+                break
+            
+        else:
+            print("Can't receive frame. Exiting ...")
+            break
+
+# Upload the pictures or videos from the inference
+def inf():
+    #Opens a dialog box for folder selection
+    Folderpath = filedialog.askdirectory()
+    #Opens a dialog box for file selection
+    Filepath = filedialog.askopenfilename()
+    #change the index to the file which selected
+    picture = cv2.VideoCapture(Filepath)
+    model=YOLO("yolov8n.pt")
+
+    if not picture.isOpened():
+        print("There is no picture/video")
+        exit()
+
+    while True:
+         # Capture frame-by-frame
+        success, frame = picture.read()
+
+        if success:
+            results = model(frame)
+            annotated_frame = results[0].plot()
+            cv2.imshow("Object Detection", annotated_frame)
+            if cv2.waitKey(1) & 0xf == ord('q'):
+                break
 
         else:
             print("Can't receive frame. Exiting ...")
             break
 
-    video.release()
-    cv2.destroyAllWindows
 
-# Upload the pictures or videos from the inference
-def inf():
-    picture = cv2.VideoCapture("inference")
 
 #create two buttons for users to choose: Live camera, Pictures or Videos
 # Create the reset button
